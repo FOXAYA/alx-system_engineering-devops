f0aa10a111253b7731339385ce21626e7fca731c
FOXAYA <omniasaged111@gmail.com>
2024-03-22 12:09:29 -0700

    EGYPT

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+#Install flask version 2.1.0
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/local/bin:/usr/bin:/bin',
+  environment => ['HOME=/root'],
+  require     => Package['python3-pip'],
+}
