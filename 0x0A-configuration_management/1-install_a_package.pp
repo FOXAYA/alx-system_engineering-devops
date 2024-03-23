4b158e74528321398063e8141f4d43ae36bd658d
FOXAYA <omniasaged111@gmail.com>
2024-03-23 14:03:03 -0700

    Welcome

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,11 @@
-#Install flask version 2.1.0
+#install flask from pip3
 
-package { 'python3-pip':
-  ensure => installed,
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
 
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  environment => ['HOME=/root'],
-  require     => Package['python3-pip'],
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
